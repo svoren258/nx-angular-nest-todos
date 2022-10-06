4bcf8ede07559e0bd8e4f88eb544b6975cc5144c
svoren258 <svoren258@gmail.com>
2022-10-06 10:20:26 +0200

    feat(app): ant design components used in the ui

--- apps/todos/src/app/app.component.ts
+++ apps/todos/src/app/app.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { Todo } from '@todos/data';
 
 @Component({
   selector: 'todos-root',
@@ -7,4 +10,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todos';
+
+  todoInputForm = this.fb.group({
+    todoTitle: ''
+  });
+
+  todos: Todo[] = [];
+
+  constructor(private readonly http: HttpClient,
+              private readonly fb: FormBuilder) {
+    this.fetch();
+  }
+
+  fetch(): void {
+    this.http.get<Todo[]>('/api/todos').subscribe((t) => (this.todos = t));
+  }
+
+  addTodo(): void {
+    this.http.post('/api/addTodo', {}).subscribe(() => {
+      this.fetch();
+    });
+  }
 }
--- apps/todos/src/app/app.module.ts
+++ apps/todos/src/app/app.module.ts
@@ -1,12 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { UiModule } from '@todos/ui';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { IconDefinition } from '@ant-design/icons-angular';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
+import { UnorderedListOutline } from '@ant-design/icons-angular/icons';
+
+const icons: IconDefinition[] = [ UnorderedListOutline ];
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent],
+  imports: [BrowserModule, HttpClientModule, UiModule, NzButtonModule, NzLayoutModule, NzIconModule.forRoot(icons), NzTypographyModule, NzFormModule, ReactiveFormsModule, NzInputModule],
   providers: [],
   bootstrap: [AppComponent],
 })
